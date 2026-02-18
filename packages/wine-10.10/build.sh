d920c1315274eb68593d26cf57198cee363f0509
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-02-18 19:00:41 -0300

    Fuck you Wine

--- packages/wine-10.10/build.sh
+++ packages/wine-10.10/build.sh
@@ -4,8 +4,8 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCH=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=ad2c2468a2cf3c39d8487bb3c08b3c4fb479d350
+GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
+GIT_COMMIT=a8ca1d6031a7bba202b32042693ca889b2e6bf22
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
